c05740fae7d7a98ee695cbaefbef4890add0366d
JoseOropeza-uru <joseoropeza@uru.edu>
2018-02-25 14:02:59 -0400

    Changes to files

--- src/jaom/org/Communicate.java
+++ src/jaom/org/Communicate.java
@@ -2,12 +2,15 @@ package jaom.org;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
 
 /**
  * Servlet implementation class Communicate
@@ -29,15 +32,20 @@ public class Communicate extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.print("{\"status\":200,\"res\":\"OK/GET\",\"param\":\""+request.getParameter("a")+"\"}");
+		JSONObject json = new JSONObject();
+		json.put("status", 200).put("res", "OK/GET").put("param", request.getParameter("a"));
+		out.println(json.toString());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		PrintWriter out = response.getWriter();
+		JSONObject json = new JSONObject(request.getReader().lines().collect(Collectors.joining()));
+		
+		json.put("status", 200).put("res", "OK/POST").put("param", request.getParameter("a"));
+		out.println(json.toString());
 	}
 
 }
